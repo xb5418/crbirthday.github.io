785a2e620417943dbc8186a170f0e125fb29402d
等待的风 <112244534+xb5418@users.noreply.github.com>
2024-07-06 15:32:37 +0800

    Update suprise.js

--- js/suprise.js
+++ js/suprise.js
@@ -235,7 +235,7 @@ S.UI = (function () {
           break;
 
         case 'icon':
-          S.ShapeBuilder.imageFile('font-awesome/' + value + '.png', function (obj) {
+          S.ShapeBuilder.imageFile('../font-awesome/' + value + '.png', function (obj) {
             S.Shape.switchShape(obj);
           });
           break;
